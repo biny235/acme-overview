0493070e542f25daf7ad84ac158a61e164fc7150
biny235 <biny235@users.noreply.github.com>
2018-01-13 19:42:37 -0500

    Finished

--- acme-prework.js
+++ acme-prework.js
@@ -1,96 +1,51 @@
-//instructions
-//write the 4 functions below
-//no third party libraries
-//try not to use any forEach
-//each function should be short and some functions can depend on other functions (hint no function should be more than 10 lines)
-
-//list of products
-var products = [
-  {
-    id: 1,
-    price: 5,
-    name: 'foo'
-  },
-  {
-    id: 2,
-    price: 3,
-    name: 'bar' 
-  },
-  {
-    id: 3,
-    price: 9,
-    name: 'bazz'
-  }
-];
-
-//list of line items
-var lineItems = [
-   {
-     productId: 1,
-     quantity: 1
-   },
-   {
-     productId: 1,
-     quantity: 1
-   },
-   {
-     productId: 2,
-     quantity: 1
-   },
-   {
-     productId: 3,
-     quantity: 1
-   },
-];
-//returns an object
-//keys are the ids of products
-//the values are the products themselves
 function generateProductsMap(products){
-  //TODO
+
+  return products.reduce(function(productsMap, product){
+    productsMap[product.id] = product;
+    return productsMap;
+  }, {});
+
 }
 
 //returns an object
 //keys are the ids of products
 //value is the total revenue for that product
 function salesByProduct(products, lineItems){
-  //TODO
+
+  return lineItems.reduce(function(totals, item){
+    
+    if(totals[item.productId]){
+      
+      totals[item.productId] += generateProductsMap(products)[item.productId].price * item.quantity;
+    }else{
+      totals[item.productId] = generateProductsMap(products)[item.productId].price * item.quantity;
+    }
+    
+    return totals;
+  }, {});
+  
 }
 
 //return the total revenue for all products
 function totalSales(products, lineItems){
   //TODO
-
+  var sales = salesByProduct(products, lineItems);
+  return products.reduce(function(total, item){
+    return total + sales[item.id];
+  }, 0);
 }
 
 //return the product responsible for the most revenue
 function topSellerByRevenue(products, lineItems){
   //TODO
+  var sales = salesByProduct(products, lineItems);
+  return products.reduce(function(greatest, item){
+    if(sales[item.id] > greatest){
+      item["sales"] = sales[item.id]
+      greatest = item;
+    }
+    return greatest
+    
+  }, 0)
+  
 }
-console.log(`generates product map - should be
-{
-  1:{
-    id: 1,
-    name: "foo",
-    price: 5
-  },
-  2:{
-    id: 2,
-    name: "bar",
-    price: 3
-  },
-  3:{
-    id: 3,
-    name: "bazz",
-    price: 9
-  }
-}
-
-`, generateProductsMap(products));
-console.log(`sales by product - should be
-  {
-    1: 10,
-    2: 3,
-    3: 9
-}`, salesByProduct( products, lineItems));
-console.log('total sales - should be 22', totalSales( products, lineItems));
-console.log('top seller by revenue', topSellerByRevenue(products, lineItems ));
